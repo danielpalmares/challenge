b8a4831fd8038e1da7905c6a608e4e128844e08c
Daniel Palmares <danielpalmares100@gmail.com>
2024-12-01 20:43:18 -0300

    feat: running more conditions, final  version ✔️

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -3,33 +3,34 @@ class RecommendationService {
     formData = { selectedPreferences: [], selectedFeatures: [] },
     products
   ) => {
+    const isSingleProduct =
+      formData.selectedRecommendationType === 'SingleProduct'
+
     const productPreferences = new Set(formData.selectedPreferences)
     const productFeatures = new Set(formData.selectedFeatures)
 
-    const productRecommendations = products.filter(product => {
-      const preferencesOutput = product.preferences.some(preference =>
-        productPreferences.has(preference)
-      )
-      const featuresOutput = product.features.some(feature =>
-        productFeatures.has(feature)
-      )
-      return preferencesOutput || featuresOutput
-    })
+    const productMatches = products
+      .map(product => {
+        const preferenceMatches = product.preferences.filter(preference =>
+          productPreferences.has(preference)
+        )
+        const featureMatches = product.features.filter(feature =>
+          productFeatures.has(feature)
+        )
 
-    return formData.selectedRecommendationType === 'SingleProduct'
-      ? productRecommendations.slice(-1)
-      : productRecommendations
+        const matchesTotal = preferenceMatches.length + featureMatches.length
+        return { ...product, matchesTotal }
+      })
+      .filter(product => product.matchesTotal > 0)
+
+    if (!isSingleProduct || productMatches.length < 1) return productMatches
+
+    const singleProductMatch = productMatches.reduce((best, product) => {
+      return product.matchesTotal >= best.matchesTotal ? product : best
+    }, productMatches[0])
+
+    return [singleProductMatch]
   }
 }
 
 export default RecommendationService
-
-/**
- * Como há apenas preferências e features, acredito que isso já é o suficiente.
- * Porém, gostaria de ressaltar outras alternativas que pensei:
- * Vamos supor que houvesse mais filtros, como range de valor...
- * Não valeria a pena seguir como foi feito, e sim, talvez unificar tudo em
- * um vetor separando por identificadores, e depois utilizar um switch para
- * validação se baseando neles, tornando assim o código bem mais clean em alta
- * escala.
- * */
